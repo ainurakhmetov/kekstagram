b54cd43496a9ae149af4d6ff2bc9f3ff1fe3c498
ainur <akhmetovainur@gmail.com>
2020-08-15 23:34:08 +0300

    module-4 task-2

--- js/pictures.js
+++ js/pictures.js
@@ -115,7 +115,7 @@ const keyCodeList = {
 const uploadCancel = document.querySelector('.img-upload__cancel');
 const uploadFile = document.querySelector('#upload-file');
 const uploadOverlay = document.querySelector('.img-upload__overlay');
-const textHashtags = document.querySelector('.text__hashtags');
+const hashtags = document.querySelector('.text__hashtags');
 const textDescription = document.querySelector('.text__description');
 
 const filtersList = document.querySelector('.effects__list');
@@ -156,7 +156,7 @@ function onBigPictureClose() {
 }
 
 function onOverlayEsc(e) {
-  if (e.keyCode === keyCodeList.ESC && document.activeElement !== textHashtags && document.activeElement !== textDescription) {
+  if (e.keyCode === keyCodeList.ESC && document.activeElement !== hashtags && document.activeElement !== textDescription) {
     onOverlayClose();
     uploadOverlay.classList.add('hidden');
   }
@@ -308,5 +308,51 @@ function onPictureMinClick(evt) {
 }
 
 pictures.addEventListener('click', onPictureMinClick);
+
+function hashtagsValidate() {
+  const tags = hashtags.value;
+  let validationError = '';
+  let duplicate = 0;
+  if (tags) {
+    const tagsArray = tags.split(' ').filter(function (el) {
+      return el !== null && el !== "" && el !== undefined;
+    });
+    if (tagsArray.length > 0) {
+      for (let i = 0; i < tagsArray.length; i++) {
+        for (let j = 0; j < tagsArray.length; j++) {
+          if (tagsArray[i].toLowerCase() === tagsArray[j].toLowerCase()) {
+            duplicate++;
+          }
+        }
+        if (duplicate > tagsArray.length) {
+          validationError = 'Хеш-тэги не должны совпадать'
+        }
+        let tag = tagsArray[i].toString().replace(/ /g, "").split('');
+        if (tag[0] === '#') {
+          if (tag.length > 1) {
+            if (tagsArray[i].split('#').length > 2) {
+              validationError ='Хеш-теги должны разделяться пробелами'
+            }
+          }
+          else if (tag.length === 1) {
+            validationError = 'Хеш-тег не может состоять только из одной решётки'
+          }
+        }
+        else {
+          validationError = 'Хеш-тег должен начинается с символа #'
+        }
+        if (tag.length > 20) {
+          validationError = 'Максимальная длина одного хэш-тега 20 символов, включая решётку'
+        }
+      }
+      if (tagsArray.length > 5) {
+        validationError = 'Нельзя указать больше пяти хэш-тегов'
+      }
+    }
+    hashtags.setCustomValidity(validationError)
+  }
+}
+
+hashtags.addEventListener('blur', hashtagsValidate);
 
 
